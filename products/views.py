52be1b5ef4f10243ad8e5c04a0905faf4b143ff8
YuriShepel <donbidonn@gmail.com>
2023-01-11 13:49:36 +0200

    Fixed MEDIA

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from products.models import Product, ProductCategory
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'Store'}
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
