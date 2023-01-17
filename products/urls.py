5fd93b13694b3f356f5bf33b2f449d089e55f4ef
YuriShepel <donbidonn@gmail.com>
2023-01-17 17:15:42 +0200

    views in products changed to CBV

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.products, name='index'),
-    path('category/<int:category_id>/', views.products, name='category'),
-    path('page//<int:page_number>/', views.products, name='paginator'),
+    path('', views.ProductsListView.as_view(), name='index'),
+    path('category/<int:category_id>/', views.ProductsListView.as_view(), name='category'),
+    path('page/<int:page>/', views.ProductsListView.as_view(), name='paginator'),
     path('baskets/add/<int:product_id>/', views.basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', views.basket_remove, name='basket_remove'),
 ]
