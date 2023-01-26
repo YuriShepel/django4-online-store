be72ecbb543f2f75e51a6d1a6b92ca852c700d22
YuriShepel <donbidonn@gmail.com>
2023-01-26 13:10:53 +0200

    Gmail verification realized

--- online_store_project/settings.py
+++ online_store_project/settings.py
@@ -135,4 +135,9 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Emails sending
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'wowvanillachannel@gmail.com'
+EMAIL_HOST_PASSWORD = ''
